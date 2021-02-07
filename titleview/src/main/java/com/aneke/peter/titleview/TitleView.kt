67f262a8dd4cb52316c97008276c3e7fe5e64a5b
petyr47 <anekepeter.dev@gmail.com>
2021-02-07 10:52:57 +0100

    update sample

--- titleview/src/main/java/com/aneke/peter/titleview/TitleView.kt
+++ titleview/src/main/java/com/aneke/peter/titleview/TitleView.kt
@@ -1,6 +1,8 @@
 package com.aneke.peter.titleview
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.View
@@ -8,7 +10,10 @@ import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.view_title.view.*
 
-class TitleView @JvmOverloads constructor(context: Context, attrs : AttributeSet?= null) : FrameLayout(context, attrs) {
+
+class TitleView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : FrameLayout(
+    context,
+    attrs) {
 
     private lateinit var attrsSet : TypedArray
 
@@ -31,8 +36,13 @@ class TitleView @JvmOverloads constructor(context: Context, attrs : AttributeSet
         attrs?.let {
             attrsSet = context.obtainStyledAttributes(attrs, R.styleable.TitleView)
             showBackButton = attrsSet.getBoolean(R.styleable.TitleView_showBackButton, true)
-            backgroundFrameColor = attrsSet.getColor(R.styleable.TitleView_backgroundColor, ContextCompat.getColor(context, android.R.color.black))
-            textColor = attrsSet.getColor(R.styleable.TitleView_textColor, ContextCompat.getColor(context, android.R.color.white))
+            backgroundFrameColor = attrsSet.getColor(R.styleable.TitleView_backgroundColor,
+                ContextCompat.getColor(
+                    context,
+                    android.R.color.black))
+            textColor = attrsSet.getColor(R.styleable.TitleView_textColor, ContextCompat.getColor(
+                context,
+                android.R.color.white))
             titleText = attrsSet.getString(R.styleable.TitleView_title) ?: ""
             drawElements()
             attrsSet.recycle()
@@ -49,6 +59,20 @@ class TitleView @JvmOverloads constructor(context: Context, attrs : AttributeSet
         } else {
             back_btn.visibility = View.VISIBLE
         }
+        back_btn.setOnClickListener {
+            getActivity()?.onBackPressed()
+        }
+    }
+
+    private fun getActivity(): Activity? {
+        var context = context
+        while (context is ContextWrapper) {
+            if (context is Activity) {
+                return context
+            }
+            context = context.baseContext
+        }
+        return null
     }
 
 
